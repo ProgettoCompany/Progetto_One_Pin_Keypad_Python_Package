3db35ef13f38174f88e97bb7b0987ae281c8244b
John Wolf <4johnryanwolf@gmail.com>
2019-02-26 16:07:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Progetto_One_Pin_Keypad",
-    version="0.0.1",
+    version="0.0.2",
     author="Progetto Company",
     author_email="progettocompany@gmail.com",
     description="Raspberry Pi compatible Python Package with examples for the One Pin Keypad.",
@@ -13,6 +13,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ProgettoCompany/Progetto_One_Pin_Keypad_Python_Package",
     packages=setuptools.find_packages(),
+    dependency_links  = ['https://github.com/adafruit/Adafruit_CircuitPython_ADS1x15/archive/1.0.2.tar.gz'],
+    install_requires  = ['adafruit-circuitpython-ads1x15>=1.0.2'],
     classifiers=[
         'Operating System :: POSIX :: Linux',
         'License :: OSI Approved :: MIT License',
